d5292a0b321fc7537a41de66edb6507bf4cf8f9e
jordantejval <jordantejval10@gmail.com>
2022-10-24 10:07:23 -0500

    Version 1

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  public image: string = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/502px-Logo_of_YouTube_%282015-2017%29.svg.png";
+  public titulo: string = "Curso de Angular con Interpolación";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
